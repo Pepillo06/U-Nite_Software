be40c9d5fbc67f87513b72a90efda5a9d47a3964
Atach0 <167770002+Atach0@users.noreply.github.com>
2026-05-01 10:29:18 -0400

    niidea

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\dev\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\PC\Desktop\U-Nite_Software\U-Nite_Software"
+export "FLUTTER_ROOT=C:\Users\refun\Downloads\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\refun\Documents\GitHub\PaginaQueNoMeAcuerdoElNombre\U-Nite_Software"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
